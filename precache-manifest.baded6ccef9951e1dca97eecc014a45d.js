2ff1714fdd21a566df0cb5623cf68d895639efae
Code Sandbox <hello@codesandbox.io>
2023-08-07 21:05:54 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.baded6ccef9951e1dca97eecc014a45d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661f14c65bd78dc0c586a14eef61d18e",
+    "url": "https://rahul-purohit1.github.io/csb-c91zqb/index.html"
+  },
+  {
+    "revision": "d7f704465c0aa207c609",
+    "url": "https://rahul-purohit1.github.io/csb-c91zqb/static/js/2.49bc9984.chunk.js"
+  },
+  {
+    "revision": "eae68b70984d424c4440",
+    "url": "https://rahul-purohit1.github.io/csb-c91zqb/static/js/main.05e52129.chunk.js"
+  },
+  {
+    "revision": "56d408925a22d8890d23",
+    "url": "https://rahul-purohit1.github.io/csb-c91zqb/static/js/runtime-main.5ddd919b.js"
+  }
+]);
